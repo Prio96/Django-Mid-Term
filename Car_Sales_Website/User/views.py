31f357aafcb4e40445c260e80e78f72d949703f0
Prio96 <prio96@icloud.com>
2024-11-16 19:08:41 +0600

    Base template made and Login View made

--- Car_Sales_Website/User/views.py
+++ Car_Sales_Website/User/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.views import LoginView
 from .forms import RegisterForm,UserDetailChange
+from django.urls import reverse_lazy
+from django.contrib import messages
 # Create your views here.
 
 def Register(request):
@@ -12,5 +14,23 @@ def Register(request):
             return redirect("HomePage")
     else:
         form=RegisterForm()
-    return render(request,'register.html',{'form':form})
+    return render(request,'register.html',{'form':form,'type':'Register'})
+
+class Login(LoginView):
+    template_name='register.html'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['type']='Login'
+        return context
+    def get_success_url(self):
+        return reverse_lazy("Profile")
+    def form_invalid(self, form):
+        messages.warning(self.request,"Invalid Credentials")
+        return super().form_invalid(form)
+    def form_valid(self, form):
+        messages.success(self.request,"Logged In Successfully")
+        return super().form_valid(form)
+
+
+
             
