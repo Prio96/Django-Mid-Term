e3a9147f6743710d73d00601333263a3ac101a9a
Prio <prio96@icloud.com>
2024-12-04 02:53:53 +0600

    Completed Car Detail Template

--- Car_Sales_Website/Car/models.py
+++ Car_Sales_Website/Car/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from Brand.models import BrandModel
 # Create your models here.
 class CarModel(models.Model):
@@ -8,7 +9,7 @@ class CarModel(models.Model):
     quantity=models.IntegerField()
     price=models.BigIntegerField()
     brand=models.ForeignKey(BrandModel,on_delete=models.CASCADE,related_name='cars')
-    
+    buyers=models.ManyToManyField(User,related_name='purchased_cars',blank=True)
 class CommentModel(models.Model):
     car=models.ForeignKey(CarModel,on_delete=models.CASCADE,related_name='comments')
     commenter=models.CharField(max_length=50)
